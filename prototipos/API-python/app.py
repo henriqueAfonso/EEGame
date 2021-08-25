259dcf2d9d2c3f5e5a0c5496b9a42c37a2e4e68e
Henrique <henriqueAfonso@users.noreply.github.com>
2021-08-25 10:40:26 -0300

    Update app.py

--- prototipos/API-python/app.py
+++ prototipos/API-python/app.py
@@ -4,13 +4,13 @@ import json
 
 class Serv(BaseHTTPRequestHandler):    
     def do_GET(self):
-        x = json.loads('{"jumpDown":true, "jumpUp":true, "horizontal":5}')        
+        controls = json.loads('{"jumpDown":true, "jumpUp":true, "horizontal":5}')        
         
         self.send_response(200)
         
         self.end_headers()
-        self.wfile.write(bytes(json.dumps(x), 'utf-8'))
+        self.wfile.write(bytes(json.dumps(controls), 'utf-8'))
 
 
-httpd = HTTPServer(('localhost', 8080), Serv)
+httpd = HTTPServer(('localhost', 3000), Serv)
 httpd.serve_forever()
