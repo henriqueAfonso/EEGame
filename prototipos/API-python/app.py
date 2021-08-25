2f6becaaf132213dbce97caff6e8dbdec5f0950d
Henrique <henriqueAfonso@users.noreply.github.com>
2021-08-25 10:38:41 -0300

    ajuste no app.py

--- prototipos/API-python/app.py
+++ prototipos/API-python/app.py
@@ -4,12 +4,12 @@ import json
 
 class Serv(BaseHTTPRequestHandler):    
     def do_GET(self):
-        x = json.dumps('{"jumpDown":true, "jumpUp":true, "horizontal":5}')        
+        x = json.loads('{"jumpDown":true, "jumpUp":true, "horizontal":5}')        
         
         self.send_response(200)
         
         self.end_headers()
-        self.wfile.write(bytes(x, 'utf-8'))
+        self.wfile.write(bytes(json.dumps(x), 'utf-8'))
 
 
 httpd = HTTPServer(('localhost', 8080), Serv)
